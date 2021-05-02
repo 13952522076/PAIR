dc9d21ba2e7891663d87b7e3c4e9b1b9a81f247e
13952522076 <xuma@my.unt.edu>
2021-05-02 16:48:31 -0400

    update

--- main.py
+++ main.py
@@ -46,8 +46,8 @@ def call_functions(args):
 
 def call_SuperResolution(args):
     print(f"Processing Super Resolution: Type:{args.SR_type} | Scale:{args.SR_scale}")
-    commands = f"python3 SuperResolution/src/main.py --data_test Demo --scale {args.SR_type} --pre_train download --test_only --save_results"
-    os.system(commands)
+    commands = f"python3 main.py --data_test Demo --scale {args.SR_type} --pre_train download --test_only --save_results"
+    os.system("cd SuperResolution/src/")
 
 def call_Deblocking(args):
     print(f"Processing Deblocking: ")
