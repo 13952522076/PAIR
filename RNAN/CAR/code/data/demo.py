b209294c843e81854c62a1291b5252d76043efbd
13952522076 <xuma@my.unt.edu>
2021-05-04 21:40:09 -0400

    update

--- RNAN/CAR/code/data/demo.py
+++ RNAN/CAR/code/data/demo.py
@@ -3,39 +3,38 @@ import os
 from data import common
 
 import numpy as np
-import scipy.misc as misc
+import imageio
 
 import torch
 import torch.utils.data as data
 
 class Demo(data.Dataset):
-    def __init__(self, args, train=False):
+    def __init__(self, args, name='Demo', train=False, benchmark=False):
         self.args = args
-        self.name = 'Demo'
+        self.name = name
         self.scale = args.scale
         self.idx_scale = 0
         self.train = False
-        self.noise_level = args.noise_level
-        apath = args.testpath + '/' + args.testset + '/Q' + str(self.noise_level[0])
+        self.benchmark = benchmark
 
         self.filelist = []
-        for f in os.listdir(apath):
-            if f.find('.png') >= 0 or f.find('.jpg') >= 0:
-                self.filelist.append(os.path.join(apath, f))
-        '''
-        for f in os.listdir(args.dir_demo):
-            if f.find('.png') >= 0 or f.find('.jp') >= 0:
-                self.filelist.append(os.path.join(args.dir_demo, f))
+        # Xu updated
+        # for f in os.listdir(args.dir_demo):
+        #     if f.find('.png') >= 0 or f.find('.jp') >= 0:
+        #         self.filelist.append(os.path.join(args.dir_demo, f))
+        # self.filelist.sort()
+        f = args.test_image
+        if f.find('.png') >= 0 or f.find('.jp') >= 0:
+            self.filelist.append(f)
         self.filelist.sort()
-        '''
 
     def __getitem__(self, idx):
-        filename = os.path.split(self.filelist[idx])[-1]
-        filename, _ = os.path.splitext(filename)
-        lr = misc.imread(self.filelist[idx])
-        lr = common.set_channel([lr], self.args.n_colors)[0]
+        filename = os.path.splitext(os.path.basename(self.filelist[idx]))[0]
+        lr = imageio.imread(self.filelist[idx])
+        lr, = common.set_channel(lr, n_channels=self.args.n_colors)
+        lr_t, = common.np2Tensor(lr, rgb_range=self.args.rgb_range)
 
-        return common.np2Tensor([lr], self.args.rgb_range)[0], -1, filename
+        return lr_t, -1, filename
 
     def __len__(self):
         return len(self.filelist)
