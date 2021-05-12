5e7d83b5da5c18041552f5ae28e00b5610db8a9c
13952522076 <xuma@my.unt.edu>
2021-05-11 22:42:49 -0400

    update

--- PAN/DN_RGB/code/option.py
+++ PAN/DN_RGB/code/option.py
@@ -24,6 +24,8 @@ parser.add_argument('--data_train', type=str, default='DIV2K',
                     help='train dataset name')
 parser.add_argument('--data_test', type=str, default='DIV2K',
                     help='test dataset name')
+parser.add_argument('--dir_demo', type=str, default='../test',
+                    help='demo image directory')
 parser.add_argument('--data_range', type=str, default='1-800/801-805',
                     help='train/test data range')
 parser.add_argument('--ext', type=str, default='sep',
